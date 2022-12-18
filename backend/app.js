dd6db74b8c34bf711badc858089931e066b0aea8
tuvantai123vn <nguyenthanhtu.cdct@gmail.com>
2022-12-18 15:38:19 +0700

    lab6.1

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const defaults = require('./routes/defaults');
 const users = require('./routes/users');
+const cors = require('cors');
+
 
 app.use(express.urlencoded({extended: false}));
 app.use(defaults);
@@ -12,4 +14,8 @@ app.use( (req, res, next) => {
 })
 
 
-app.listen(3000);
+app.use(cors());
+
+ app.listen(4000, function () {
+  console.log('CORS-enabled web server listening on port 4000')
+});
